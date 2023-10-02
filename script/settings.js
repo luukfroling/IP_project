badbecbff04df393631aff5322375a3ebb7bbf80
luukfroling <31106485+luukfroling@users.noreply.github.com>
2023-10-02 22:24:39 +0200

    Update settings.js

--- script/settings.js
+++ script/settings.js
@@ -5,13 +5,13 @@ let settings = {
     time_per_penalty: 1,
     time_per_hint: 5,
     hint_q1: "Doe eens normaal",
-    hint_q2: "Zoals het artikel van Pols over Versnelling langs de helling: 40(4)",
-    hint_q3: "De oplossing is niet ver",
+    hint_q2: "Zoals het artikel van Pols over Versnelling langs de helling: 40(4), 48(6)",
+    hint_q3: "De oplossing is niet ver, 1,2,6",
     image_question_1: "",
     text_question_2: "Nienke Lurvink schrijft over escaperooms in de klas. Een referentie naar haar artikel bevat zowel de jaargang als de editie.",
     text_question_3: "this is going to be question 3, to be filled in late, just have to get some testing text now.",
     answer_1: "cbda",
-    answer_2: "cbda",
+    answer_2: "48(6)",
     answer_3: [true, true, false, false, false, true]
 }
 
